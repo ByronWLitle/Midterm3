ccda42ede1186d3ae47de59639b2cf01c7cce747
ByronWLitle <bwlitle@amarillocollege.com>
2024-09-09 14:37:30 -0500

    Added comments and output that shows user they guessed correctly

--- Midterm3/Program.cs
+++ Midterm3/Program.cs
@@ -10,22 +10,32 @@ namespace Midterm3
     {
         static void Main(string[] args)
         {
-            int guess = 0;
-            int num = 75;
+            //Initialize variables to hold values
+            int guess = 0; //User input
+            int num = 75; //Number in which they have to guess
+            //Repeats until guess is equal to number
             while (guess != num)
             {
-                Console.WriteLine("Guess a number");
+                //Prompts user for guess
+                Console.Write("Guess a number between 0-100: ");
+                //Converts user input to integer variable and store in memory
                 guess = Convert.ToInt32(Console.ReadLine());
-
+                //If-else statement that gives certain outputs to help the user guess number
                 if (guess > num)
                 {
+                    //Outputs "too high" if guess is higher than number
                     Console.WriteLine("Too high!");
                 }
                 else if (guess < num)
                 {
+                    //Outputs "too low" if guess is lower than number
                     Console.WriteLine("Too low!");
                 }
             }
+            //Outputs user that guess is correct
+            Console.WriteLine("You got it!");
+            //Pauses program to allow user to view before program ends
+            Console.ReadLine();
         }
     }
 }
